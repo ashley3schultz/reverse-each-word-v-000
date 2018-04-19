4431261ca86d0ff3afd8f9e2c362909730f83c3d
Ashley Schultz <ashleyeschultz@gmail.com>
2018-04-19 18:51:02 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   array = sentence.split
   narray = []
-  array.each {|word| narray << word.reverse}
+  array.collect {|word| narray << word.reverse}
   narray.join(" ")
 end
